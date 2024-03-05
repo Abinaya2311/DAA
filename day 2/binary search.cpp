1472b394f3decc5afec531ff880c2660f0f482d6
192211241 <122424318+Abinaya2311@users.noreply.github.com>
2024-03-05 23:34:45 +0530

    Create binary search.cpp

---
+++ day 2/binary search.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(int arr[], int size, int target) {
+    int left = 0, right = size - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) return mid;
+        else if (arr[mid] < target) left = mid + 1;
+        else right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 7;
+    int index = binarySearch(arr, size, target);
+    if (index != -1)
+        cout << "Element found at index: " << index << endl;
+    else
+        cout << "Element not found" << endl;
+    return 0;
+}
